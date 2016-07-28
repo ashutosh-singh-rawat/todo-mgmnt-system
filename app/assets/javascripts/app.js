d65ee5d4251ff0d47377610201a4b38f833064aa
Ashutosh Rawat <singh.ashutoshrawat@gmail.com>
2016-07-29 04:02:56 +0530

    fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -20,7 +20,10 @@ app.config([
     .state('dashboard', {
       url: '/dashboard',
       templateUrl: 'views/dashboard/_dashboard.html',
-      controller: 'DashboardCtrl'
+      controller: 'DashboardCtrl',
+      resolve: {
+        loginRequired: ["User", function(User){ return User.loginRequired();}]
+      }
     })
     .state('login', {
       url: '/login',
@@ -36,16 +39,18 @@ app.config([
       url: '/register',
       templateUrl: 'views/authentication/_register.html',
       controller: 'AuthenticationCtrl',
-      resolve: {
-        skipIfLoggedIn: skipIfLoggedIn
-      }
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+        Auth.currentUser().then(function (){
+          $state.go('dashboard');
+        })
+      }]
     })
     .state('new-project', {
       url: '/new-project',
       templateUrl: 'views/projects/_new-project.html',
       controller: 'ProjectsCtrl',
       resolve: {
-        access: loginRequired
+        loginRequired: ["User", function(User){ return User.loginRequired();}]
       }
     })
     .state('new-todo', {
@@ -53,7 +58,7 @@ app.config([
       templateUrl: 'views/projects/_new-todo.html',
       controller: 'ProjectsCtrl',
       resolve: {
-        access: loginRequired
+        loginRequired: ["User", function(User){ return User.loginRequired();}]
       }
     })
     .state('assign-todo', {
@@ -61,14 +66,14 @@ app.config([
       templateUrl: 'views/projects/_assign-todo.html',
       controller: 'ProjectsCtrl',
       resolve: {
-        access: loginRequired
+        loginRequired: ["User", function(User){ return User.loginRequired();}]
       }
     }).state('assign-developer', {
       url: '/assign-developer',
       templateUrl: 'views/projects/_assign-developer.html',
       controller: 'ProjectsCtrl',
       resolve: {
-        access: loginRequired
+        loginRequired: ["User", function(User){ return User.loginRequired();}]
       }
     });
 
@@ -90,13 +95,19 @@ app.run(['$rootScope',
   'Notifier',
   'Auth',
   function($rootScope, $state, Notifier, Auth){
-    $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error){
-      if(error == "login_required"){
+    $rootScope.$on("$stateChangeError", function(event, toState, toParams, fromState, fromParams, error) {
+      if(error == "login_required") {
         // Notifier.notifyWarning("You need to be logged in order to view this page");
         $state.go("login");
-      }else if(error == "skip_login"){
+      } else if(error == "skip_login"){
         $state.go(fromState);
       }
     });
+    // $rootScope.$on('$routeChangeStart', function(event, next, current){
+    //   if(next.$$route.require_login == false && !Auth.isAuthenticated()) {
+    //     $location.path("/login");
+    //   }
+    // });
+
   }
 ]);
